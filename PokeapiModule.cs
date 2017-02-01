d08f64e8681869f68439ca3921356c33f03e62e7
Donovan An <donovantaean@yahoo.com>
2017-02-01 13:35:48 -0800

    displaying weight, height, name

--- PokeapiModule.cs
+++ PokeapiModule.cs
@@ -1,5 +1,6 @@
 using Nancy;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using ApiCaller;
 namespace HelloNancy
@@ -9,10 +10,16 @@ namespace HelloNancy
         public PokeapiModule()
         
         {
-            // Get("/", args => {
-            //     System.Console.WriteLine("IN ROOT");
-            //     return View["index"]; 
-            // });
+            Get("/", args => {
+                System.Console.WriteLine("IN ROOT");
+                string Name = "?";
+                string Weight = "?";
+                string Height = "?";
+                ViewBag.name = Name;
+                ViewBag.weight = Weight;
+                ViewBag.height = Height;
+                return View["index"]; 
+            });
             Get("/{num}", async args =>{
                 string Name = "";
                 long Weight = 0;
@@ -21,7 +28,7 @@ namespace HelloNancy
                 // Our anonymous function is a parameter of type Action that returns a Dictionary
                 await WebRequest.SendRequest("http://pokeapi.co/api/v2/pokemon/"+pokeID, new Action<Dictionary<string, object>>( JsonResponse =>
                     {
-                        // System.Console.WriteLine(JsonResponse["types"]);
+                        // System.Console.WriteLine((Array)JsonResponse["types"]);
                         Name = (string)JsonResponse["name"];
                         Weight = (long)JsonResponse["weight"];    
                         Height = (long)JsonResponse["height"];
